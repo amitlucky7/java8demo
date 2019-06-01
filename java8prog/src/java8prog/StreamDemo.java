202e4b02ba88c3a65041275051e07a02b97aae37
DELL <DELL@DELL-PC>
2019-06-02 01:17:51 +0530

    adding StreamDemo.java

---
+++ java8prog/src/java8prog/StreamDemo.java
@@ -0,0 +1,21 @@
+package java8prog;
+
+import java.util.*;
+
+public class StreamDemo {
+
+	public static void main(String[] args) {
+		Map<String,Integer> map = new HashMap<>();
+		map.put("amit", 4);
+		map.put("harsh", 3);
+		map.put("jaya", 1);
+		map.put("rahul", 2);
+		
+		arrangeOrder(map);
+	}
+
+	private static void arrangeOrder(Map<String, Integer> map) {
+		// TODO Auto-generated method stub
+		
+	}
+}
